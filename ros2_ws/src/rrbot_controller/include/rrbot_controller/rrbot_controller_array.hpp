e447e67f50a9f39b2847f1620b5f187be20ad454
Bence Magyar <bence.magyar.robotics@gmail.com>
2021-06-01 16:07:18 +0100

    Add array-based controller variant

---
+++ ros2_ws/src/rrbot_controller/include/rrbot_controller/rrbot_controller_array.hpp
@@ -0,0 +1,78 @@
+// Copyright (c) 2021, Bence Magyar and Denis Stogl
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef RRBOT_CONTROLLER__RRBOT_CONTROLLER_HPP_
+#define RRBOT_CONTROLLER__RRBOT_CONTROLLER_HPP_
+
+#include <string>
+#include <vector>
+
+#include "rrbot_controller/visibility_control.h"
+#include "controller_interface/controller_interface.hpp"
+#include "rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp"
+#include "rclcpp_lifecycle/state.hpp"
+#include "realtime_tools/realtime_buffer.h"
+#include "realtime_tools/realtime_publisher.h"
+
+// TODO(anyone): Replace with controller specific messages
+#include "control_msgs/msg/joint_controller_state.hpp"
+#include "example_interfaces/msg/float64_multi_array.hpp"
+
+namespace rrbot_controller
+{
+using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
+
+class RRBotControllerArray : public controller_interface::ControllerInterface
+{
+public:
+  RRBOT_CONTROLLER_PUBLIC
+  controller_interface::return_type init(const std::string & controller_name) override;
+
+  RRBOT_CONTROLLER_PUBLIC
+  controller_interface::InterfaceConfiguration command_interface_configuration() const override;
+
+  RRBOT_CONTROLLER_PUBLIC
+  controller_interface::InterfaceConfiguration state_interface_configuration() const override;
+
+  RRBOT_CONTROLLER_PUBLIC
+  CallbackReturn on_configure(const rclcpp_lifecycle::State & previous_state) override;
+
+  RRBOT_CONTROLLER_PUBLIC
+  CallbackReturn on_activate(const rclcpp_lifecycle::State & previous_state) override;
+
+  RRBOT_CONTROLLER_PUBLIC
+  CallbackReturn on_deactivate(const rclcpp_lifecycle::State & previous_state) override;
+
+  RRBOT_CONTROLLER_PUBLIC
+  controller_interface::return_type update() override;
+
+protected:
+  std::vector<std::string> joint_names_;
+  std::string interface_name_;
+
+  using ControllerCommandMsg = example_interfaces::msg::Float64MultiArray;
+
+  rclcpp::Subscription<ControllerCommandMsg>::SharedPtr command_subscriber_ = nullptr;
+  realtime_tools::RealtimeBuffer<std::shared_ptr<ControllerCommandMsg>> input_command_;
+
+  using ControllerStateMsg = control_msgs::msg::JointControllerState;
+  using ControllerStatePublisher = realtime_tools::RealtimePublisher<ControllerStateMsg>;
+
+  rclcpp::Publisher<ControllerStateMsg>::SharedPtr s_publisher_;
+  std::unique_ptr<ControllerStatePublisher> state_publisher_;
+};
+
+}  // namespace rrbot_controller
+
+#endif  // RRBOT_CONTROLLER__RRBOT_CONTROLLER_HPP_
