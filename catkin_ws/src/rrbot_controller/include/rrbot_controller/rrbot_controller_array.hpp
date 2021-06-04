b444edcf1f8b0271068e01b9e2a410358a4507e4
Denis Štogl <denis@stogl.de>
2021-06-04 11:46:39 +0200

    Array controller is compiling. Not yet fully implemented

---
+++ catkin_ws/src/rrbot_controller/include/rrbot_controller/rrbot_controller_array.hpp
@@ -0,0 +1,58 @@
+// Copyright (c) 2021, Bence Magyar and Denis Stogl
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef RRBOT_CONTROLLER__RRBOT_CONTROLLER_ARRAY_HPP_
+#define RRBOT_CONTROLLER__RRBOT_CONTROLLER_ARRAY_HPP_
+
+#include <string>
+#include <vector>
+
+#include <hardware_interface/joint_command_interface.h>
+#include <controller_interface/controller.h>
+#include <realtime_tools/realtime_buffer.h>
+#include <ros/node_handle.h>
+
+#include <control_msgs/JointControllerState.h>
+#include <std_msgs/Float64MultiArray.h>
+
+namespace rrbot_controller
+{
+class RRBotControllerArray :
+public controller_interface::Controller<hardware_interface::PositionJointInterface>
+{
+public:
+  bool init(hardware_interface::PositionJointInterface * hw, ros::NodeHandle & nh);
+
+  void starting(const ros::Time & time);
+
+  void update(const ros::Time & time, const ros::Duration & period);
+
+protected:
+  std::vector<std::string> joint_names_;
+  std::vector< hardware_interface::JointHandle > joints_;
+  realtime_tools::RealtimeBuffer<std::vector<double> > commands_buffer_;
+
+  using ControllerCommandMsg = std_msgs::Float64MultiArray;
+
+  ros::Subscriber command_subscriber_;
+  void commandCB(const ControllerCommandMsg::ConstPtr & msg);
+
+  using ControllerStateMsg = control_msgs::JointControllerState;
+//   using ControllerStatePublisher = realtime_tools::RealtimePublisher<ControllerStateMsg>;
+
+};
+
+}  // namespace rrbot_controller
+
+#endif  // RRBOT_CONTROLLER__RRBOT_CONTROLLER_ARRAY_HPP_
---
+++ catkin_ws/src/rrbot_controller/src/rrbot_controller_array.cpp
@@ -0,0 +1,86 @@
+// Copyright (c) 2021, Bence Magyar and Denis Stogl
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <rrbot_controller/rrbot_controller_array.hpp>
+
+namespace rrbot_controller
+{
+
+bool RRBotControllerArray::init(hardware_interface::PositionJointInterface * hw, ros::NodeHandle & nh)
+{
+  // List of controlled joints
+  std::string param_name = "joints";
+  if(!nh.getParam(param_name, joint_names_))
+  {
+    ROS_ERROR_STREAM("Failed to getParam '" << param_name << "' (namespace: " << nh.getNamespace() << ").");
+    return false;
+  }
+  size_t num_joints = joint_names_.size();
+
+  if (num_joints == 0) {
+    ROS_ERROR_STREAM("List of joint names is empty.");
+    return false;
+  }
+  for(size_t i = 0; i<num_joints; ++i)
+  {
+    try
+    {
+      joints_.push_back(hw->getHandle(joint_names_[i]));
+    }
+    catch (const hardware_interface::HardwareInterfaceException& e)
+    {
+      ROS_ERROR_STREAM("Exception thrown: " << e.what());
+      return false;
+    }
+  }
+
+  commands_buffer_.writeFromNonRT(std::vector<double>(num_joints, 0.0));
+
+  command_subscriber_ = nh.subscribe<ControllerCommandMsg>("command", 1, &RRBotControllerArray::commandCB, this);
+  return true;
+}
+
+void RRBotControllerArray::starting(const ros::Time & /*time*/)
+{
+  // Start controller with current joint positions
+  std::vector<double> & commands = *commands_buffer_.readFromRT();
+  for(size_t i = 0; i< joint_names_.size(); ++i)
+  {
+    commands[i]=joints_[i].getPosition();
+  }
+}
+
+void RRBotControllerArray::update(const ros::Time & /*time*/, const ros::Duration & /*period*/)
+{
+  std::vector<double> & commands = *commands_buffer_.readFromRT();
+  for(size_t i = 0; i < joint_names_.size(); ++i) {
+    joints_[i].setCommand(commands[i]);
+  }
+}
+
+void RRBotControllerArray::commandCB(const ControllerCommandMsg::ConstPtr & msg)
+{
+  if (msg->data.size() != joint_names_.size())
+  {
+//     ROS_ERROR_STREAM("Dimension of command (" << msg->data.size() << ") does not match number of joints (" << joint_names_.size() << ")! Not executing!");
+    return;
+  }
+  commands_buffer_.writeFromNonRT(msg->data);
+}
+
+}  // namespace rrbot_controller
+
+#include <pluginlib/class_list_macros.hpp>
+
+PLUGINLIB_EXPORT_CLASS(rrbot_controller::RRBotControllerArray, controller_interface::ControllerBase)
